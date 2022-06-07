edfe4c99aff49ccc294e6c57e997b542d83e338c
hfqqwe <2120646522@qq.com>
2022-06-07 23:01:33 +0800

    新增切换城市和手动更新天气的功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.exampl.coolweather.R;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -60,9 +63,25 @@ private City selectedCity;
 * 当前选中的级别
 */
 private int currentLevel;
+/**
+* 是否从WeatherActivity中跳转过来。
+*/
+private boolean isFromWeatherActivity;
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 super.onCreate(savedInstanceState);
+isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+SharedPreferences prefs = PreferenceManager.
+getDefaultSharedPreferences(this);
+// 已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+
+if (prefs.getBoolean("city_selected", false)
+&& !isFromWeatherActivity) {
+Intent intent = new Intent(this, WeatherActivity.class);
+startActivity(intent);
+finish();
+return;
+}
 requestWindowFeature(Window.FEATURE_NO_TITLE);
 setContentView(R.layout.choose_area);
 listView = (ListView) findViewById(R.id.list_view);
@@ -81,6 +100,13 @@ queryCities();
 } else if (currentLevel == LEVEL_CITY) {
 selectedCity = cityList.get(index);
 queryCounties();
+} else if (currentLevel == LEVEL_COUNTY) {
+String countyCode = countyList.get(index).getCountyCode();
+Intent intent = new Intent(ChooseAreaActivity.this,
+WeatherActivity.class);
+intent.putExtra("county_code", countyCode);
+startActivity(intent);
+finish();
 }
 }
 });
@@ -225,6 +251,7 @@ progressDialog.dismiss();
 第一行代码——Android
 506
 */
+
 @Override
 public void onBackPressed() {
 if (currentLevel == LEVEL_COUNTY) {
@@ -232,6 +259,10 @@ queryCities();
 } else if (currentLevel == LEVEL_CITY) {
 queryProvinces();
 } else {
+if (isFromWeatherActivity) {
+Intent intent = new Intent(this, WeatherActivity.class);
+startActivity(intent);
+}
 finish();
 }
 }
